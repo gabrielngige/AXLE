11cc523325509703e210db937292849a83d3dd1b
gabriel <gabriel.ngige@student.moringaschool.com>
2026-03-17 12:09:22 +0300

    javascript trial lesson and first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let grade = 44
+
+if (grade >= 79) {
+  console.log("Grade A");
+} else if (grade >= 65) {
+  console.log("Grade B");
+} else if (grade >= 55) {
+  console.log("Grade C");
+} else if (grade >= 30) {
+  console.log("Grade D");
+} else {
+  console.log("Grade F");
+}
